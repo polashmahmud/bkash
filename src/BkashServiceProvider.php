5b3c309468d01ef9955f3ad20e33fc06323a42d1
Polash Mahmud <polashmahmud@gmail.com>
2024-01-23 23:21:21 +0600

    setup package files

---
+++ src/BkashServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Polashmahmud\Bkash;
+
+use Illuminate\Support\ServiceProvider;
+
+class BkashServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+    }
+}
---
+++ src/Models/Bkash.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Polashmahmud\Bkash\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bkash extends Model
+{
+    use HasFactory;
+}
