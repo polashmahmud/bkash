ba015ef141007273edb0ef28d3b018c93c5ee0e5
Polash Mahmud <polashmahmud@gmail.com>
2024-01-24 09:12:38 +0600

    working on bkash

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,7 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/bkash', function () {
+    return 'Hello from the bkash package!';
+})->name('bkash');
--- src/BkashServiceProvider.php
+++ src/BkashServiceProvider.php
@@ -20,5 +20,6 @@ class BkashServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
     }
 }
