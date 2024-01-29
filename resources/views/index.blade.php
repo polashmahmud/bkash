27e1cd49b2138487b2dbcb3330405fdf0c639076
Polash Mahmud <polashmahmud@gmail.com>
2024-01-29 19:54:07 +0600

    modify show page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,5 @@
 <x-bkash::layouts.app>
     <div class="mx-auto max-w-screen-xl space-y-6 py-12">
-
         <x-bkash::summary-card :total="$total" :total-transactions="$totalTransactions" :period="$period"/>
 
         <x-bkash::summary-chart :sums="$sums" :dates="$dates" :period="$period"/>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,148 +1,180 @@
-<x-bkash::layout>
-    <div class="mx-auto max-w-2xl flex justify-center space-y-6 py-12">
-        <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <div class="flex items-center justify-between mb-4">
-                <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Payment Details</h5>
-                <a href="/bkash" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-                    Back to list
-                </a>
-            </div>
-            <div class="flow-root">
-                <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Payment Id
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->paymentID }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Trx ID
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->trxID }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Transaction Status
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->transactionStatus }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Amount
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->amount }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Currency
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->currency }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Intent
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->intent }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Payment Execute Time
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->paymentExecuteTime }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Merchant Invoice Number
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->merchantInvoiceNumber }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Payer Reference
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->payerReference }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Status Code
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->statusCode }}
-                            </div>
-                        </div>
-                    </li>
-                    <li class="py-3 sm:py-4">
-                        <div class="flex items-center">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                    Status Message
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                {{ $bkash->statusMessage }}
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+<x-bkash::layouts.app>
+    <div class="mx-auto max-w-screen-xl flex justify-center space-y-6 py-12">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                        Payment Details
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Details
+                    </th>
+                    <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
+                        Payment Refund Details
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Details
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Payment Id
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->paymentID }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Trax ID
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundTrxID }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Trx ID
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->trxID }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Transaction Status
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundTransactionStatus }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Transaction Status
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->transactionStatus }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Amount
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundAmount }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Amount
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->amount }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Currency
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundCurrency }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Currency
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->currency }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Charge
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundCharge }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Intent
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->intent }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Completed Time
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundCompletedTime }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Payment Execute Time
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->paymentExecuteTime }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Status Code
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundStatusCode }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Merchant Invoice Number
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->merchantInvoiceNumber }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+                        Refund Status Message
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $bkash->refundStatusMessage }}
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Payer Reference
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->payerReference }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+
+                    </td>
+                    <td class="px-6 py-4">
+
+                    </td>
+                </tr>
+                <tr class="border-b border-gray-200 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Status Code
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->statusCode }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+
+                    </td>
+                    <td class="px-6 py-4">
+
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                        Status Message
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $bkash->statusMessage }}
+                    </td>
+                    <td class="px-6 py-4 bg-gray-50 dark:bg-gray-800">
+
+                    </td>
+                    <td class="px-6 py-4">
+
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
-</x-bkash::layout>
+</x-bkash::layouts.app>
