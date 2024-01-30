9c2a07e083bb3110f6d8434b26acae8a2f11bf64
Polash Mahmud <polashmahmud@gmail.com>
2024-01-30 21:12:31 +0600

    modify config

--- config/bkash.php
+++ config/bkash.php
@@ -1,13 +1,21 @@
 <?php
 
-/*
- * You can place your bkash configuration in here.
- */
-
 return [
-    'base_url'   => 'https://tokenized.sandbox.bka.sh/v1.2.0-beta',
-    'app_key'    => '',
-    'app_secret' => '',
-    'username'   => '',
-    'password'   => ''
+
+    /*
+   |--------------------------------------------------------------------------
+   | Bkash Configurations
+   |--------------------------------------------------------------------------
+   |
+   | This option controls the default bkash configurations that will be used
+   | by the application. You can set this to a sensible default value as
+   | you may easily change it throughout the application.
+   |
+   */
+
+    'base_url'   => env('BKASH_BASE_URL', 'https://tokenized.sandbox.bka.sh/v1.2.0-beta'),
+    'app_key'    => env('BKASH_APP_KEY', ''),
+    'app_secret' => env('BKASH_APP_SECRET', ''),
+    'username'   => env('BKASH_USER_NAME', ''),
+    'password'   => env('BKASH_PASSWORD', '')
 ];
